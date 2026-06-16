761830cd2ce45b486b11545e8475ce73c197340d
Enzo Coutinho Barbosa <enzocoutinhobarbosa@gmail.com>
2026-06-16 02:03:56 -0300

    envio de email funcionando, sessão de contato quase completa e index sincronizado com js.

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const contatoForm = document.getElementById("contato-form"),
-  contatomensagem = document.getElementById("contato__mensagem");
+  contatoMensagem = document.getElementById("contato__mensagem");
 
 const sendEmail = (e) => {
   e.preventDefault();
+
+  emailjs
+    .sendForm(
+      "service_iadyxwl",
+      "template_wi2qifw",
+      "#contato-form",
+      "PKNxT9V2OTmkZX96v",
+    )
+    .then(() => {
+      contatoMensagem.textContent = "Mensagem enviada com sucesso ✔";
+      contatoForm.reset();
+      setTimeout(() => {
+        contatoMensagem.textContent = "";
+      }, 5000);
+    })
+    .catch((erro) => {
+      contatoMensagem.textContent = "Erro ao enviar a mensagem ❌";
+      console.log("Erro do EmailJS:", erro);
+    });
 };
 
 contatoForm.addEventListener("submit", sendEmail);
